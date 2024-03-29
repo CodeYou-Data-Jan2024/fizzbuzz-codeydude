1eb2e5cbc8e28ec8b967241e3c8bcbb949db3131
Lavin Lalchandani <lavinl27@gmail.com>
2024-03-29 18:04:45 -0400

    assignment completed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
 # add your code here
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "Fizzbuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+    
+for i in range(1,100):
+    print(fizzbuzz(i))
 
