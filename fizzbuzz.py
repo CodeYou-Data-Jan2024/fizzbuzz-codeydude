1d7822e061ad55a36c3facb8ea6f300f815e1d57
Lavin Lalchandani <lavinl27@gmail.com>
2024-03-30 11:14:16 -0400

    redid the code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,25 @@
-def fizzbuzz(number):
-    if number % 3 == 0 and number % 5 == 0:
-        return "Fizzbuzz"
-    elif number % 3 == 0:
-        return "Fizz"
-    elif number % 5 == 0:
-        return "Buzz"
-    else:
-        return number
+# def fizzbuzz(number):
+#     if number % 3 == 0 and number % 5 == 0:
+#         return "Fizzbuzz"
+#     elif number % 3 == 0:
+#         return "Fizz"
+#     elif number % 5 == 0:
+#         return "Buzz"
+#     else:
+#         return number
     
-for i in range(1,101):
-    print(fizzbuzz(i))
+# for i in range(1,101):
+#     print(fizzbuzz(i))
+
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
 
 
+    
